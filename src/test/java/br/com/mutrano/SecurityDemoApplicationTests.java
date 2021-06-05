3424df92a72aa9373f910cb73d88a1a1a3d8237c
Mario Rabelo <mutrano@gmail.com>
2021-06-05 17:24:20 -0300

    Started project

---
+++ src/test/java/br/com/mutrano/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mutrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
